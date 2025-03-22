1eaed680c067d441c96ec2bb498dbae0ad1581f7
Lanvizu <ghzm888@gmail.com>
2025-03-22 20:13:11 +0900

    feat: 기본 home 컨트롤러 생성

---
+++ src/main/java/OUA/OUA_V1/HomeController.java
@@ -0,0 +1,15 @@
+package OUA.OUA_V1;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public ResponseEntity<String> home() {
+        return ResponseEntity.ok("Welcome to OUA_V1!");
+    }
+}
