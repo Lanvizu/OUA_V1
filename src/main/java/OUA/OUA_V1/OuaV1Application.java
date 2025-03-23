4a99c5e384325c893865024374b2c1e251827cba
이준호 <ghzm888@gmail.com>
2025-03-24 01:26:45 +0900

    feat: jwt 스캔 추가

--- src/main/java/OUA/OUA_V1/OuaV1Application.java
+++ src/main/java/OUA/OUA_V1/OuaV1Application.java
@@ -2,8 +2,10 @@ package OUA.OUA_V1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class OuaV1Application {
 
 	public static void main(String[] args) {
