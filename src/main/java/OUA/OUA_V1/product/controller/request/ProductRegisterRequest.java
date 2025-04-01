24aff80f78f1c343cc9e524353db480ac540e697
이준호 <ghzm888@gmail.com>
2025-04-01 20:33:05 +0900

    feat: Product DTO 추가 (request)

---
+++ src/main/java/OUA/OUA_V1/product/controller/request/ProductRegisterRequest.java
@@ -0,0 +1,24 @@
+package OUA.OUA_V1.product.controller.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDateTime;
+
+public record ProductRegisterRequest(
+        @NotBlank(message = "상품 이름을 입력해주세요.")
+        String name,
+
+        @NotBlank(message = "상품 설명을 입력해주세요.")
+        String description,
+
+        @NotBlank(message = "경매 시작 가격을 입력해주세요.")
+        int initialPrice,
+
+        @NotBlank(message = "즉시 구매가를 입력해주세요.")
+        int buyNowPrice,
+
+        @NotBlank(message = "경매 종료 시간을 설정해주세요")
+        LocalDateTime endDate
+
+) {
+}
