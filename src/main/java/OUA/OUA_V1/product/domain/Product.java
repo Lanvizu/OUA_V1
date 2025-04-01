6e3b8fcc9c2d7ea90ca1b91099fb0d31d2fc226a
이준호 <ghzm888@gmail.com>
2025-04-01 20:30:46 +0900

    feat: Product 엔티티 추가

---
+++ src/main/java/OUA/OUA_V1/product/domain/Product.java
@@ -0,0 +1,117 @@
+package OUA.OUA_V1.product.domain;
+
+import OUA.OUA_V1.BaseEntity;
+import OUA.OUA_V1.member.domain.Member;
+import OUA.OUA_V1.product.exception.badRequest.ProductIllegalNameException;
+import OUA.OUA_V1.product.exception.badRequest.ProductNameBlankException;
+import OUA.OUA_V1.product.exception.badRequest.ProductNameLengthException;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class Product extends BaseEntity {
+
+    private static final int NAME_MIN_LENGTH = 2;
+    private static final int NAME_MAX_LENGTH = 32;
+    private static final Pattern NAME_PATTERN = Pattern.compile("^[^\\\\|]*$");
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private int initialPrice;
+
+    private int buyNowPrice;
+
+    @Column(name = "start_date")
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date")
+    private LocalDateTime endDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ProductStatus status;
+
+    @ElementCollection
+    @CollectionTable(name = "product_images", joinColumns = @JoinColumn(name = "product_id"))
+    @Column(name = "image_urls")
+    private List<String> imageUrls = new ArrayList<>();
+
+    //즐겨찾기 수 추후 추가
+
+    public Product(Member member, String name, String description, int initialPrice, int buyNowPrice, LocalDateTime endDate, List<String> imageUrls) {
+        this.member = member;
+        validateName(name);
+        this.name = name;
+        this.description = description;
+        this.initialPrice = initialPrice;
+        this.buyNowPrice = buyNowPrice;
+        this.status = ProductStatus.FOR_SALE;
+        this.startDate = LocalDateTime.now();
+        this.endDate = endDate;
+        this.imageUrls = imageUrls;
+    }
+
+    private void validateName(String name) {
+        if (name.isEmpty()) {
+            throw new ProductNameBlankException();
+        }
+        if (name.length() < NAME_MIN_LENGTH || name.length() > NAME_MAX_LENGTH) {
+            throw new ProductNameLengthException(NAME_MIN_LENGTH, NAME_MAX_LENGTH, name.length());
+        }
+        if (!NAME_PATTERN.matcher(name).matches()) {
+            throw new ProductIllegalNameException();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Product product)) {
+            return false;
+        }
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", initialPrice='" + initialPrice + '\'' +
+                ", buyNowPrice='" + buyNowPrice + '\'' +
+                ", endDate='" + endDate + '\'' +
+                '}';
+    }
+}
