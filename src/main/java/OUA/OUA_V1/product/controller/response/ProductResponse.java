e9b925196a671d80f05af51db3791b302ba25b39
이준호 <ghzm888@gmail.com>
2025-04-03 16:38:33 +0900

    feat: response용 prodcut DTO 추가

---
+++ src/main/java/OUA/OUA_V1/product/controller/response/ProductResponse.java
@@ -0,0 +1,15 @@
+package OUA.OUA_V1.product.controller.response;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ProductResponse(
+        Long productId,
+        String name,
+        String description,
+        int initialPrice,
+        int buyNowPrice,
+        LocalDateTime endDate,
+        List<String> imageUrls
+) {
+}
