fc091c067b882f20921a3c072eb47399d579d95f
이준호 <ghzm888@gmail.com>
2025-04-10 15:29:02 +0900

    feat: updatePassword 관련 api 추가

--- backend/src/main/java/OUA/OUA_V1/member/controller/MemberController.java
+++ backend/src/main/java/OUA/OUA_V1/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package OUA.OUA_V1.member.controller;
 import OUA.OUA_V1.member.controller.request.CodeVerificationRequest;
 import OUA.OUA_V1.member.controller.request.EmailRequest;
 import OUA.OUA_V1.member.controller.request.MemberCreateRequest;
+import OUA.OUA_V1.member.controller.request.NewPasswordRequest;
 import OUA.OUA_V1.member.controller.response.VerificationResponse;
 import OUA.OUA_V1.member.facade.MemberFacade;
 import jakarta.validation.Valid;
@@ -20,8 +21,14 @@ public class MemberController {
     private final MemberFacade memberFacade;
 
     @PostMapping("/email-verification")
-    public ResponseEntity<Void> requestEmailVerification(@RequestBody @Valid EmailRequest request) {
+    public ResponseEntity<Void> requestSignUpEmailVerification(@RequestBody @Valid EmailRequest request) {
         memberFacade.requestEmailVerification(request);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/password-update-email-verification")
+    public ResponseEntity<Void> requestPasswordUpdateEmailVerification(@RequestBody @Valid EmailRequest request) {
+        memberFacade.requestPasswordUpdateEmailVerification(request);
         return ResponseEntity.ok().build();
     }
 
@@ -36,5 +43,12 @@ public class MemberController {
                                        @CookieValue(name = "authToken", required = true) String token) {
         Long memberId = memberFacade.create(request, token);
         return ResponseEntity.created(URI.create("/v1/members/" + memberId)).build();
+    }
+
+    @PostMapping("/update-password")
+    public ResponseEntity<Void> updatePassword(@RequestBody @Valid NewPasswordRequest request,
+                                               @CookieValue(name = "authToken", required = true) String token) {
+        memberFacade.updatePassword(request, token);
+        return ResponseEntity.ok().build();
     }
 }
