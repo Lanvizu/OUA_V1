0ead8c7c4f4491ab099d7111522ce0fc5f75eba6
이준호 <ghzm888@gmail.com>
2025-04-18 13:56:03 +0900

    feat: Order 엔티티 클래스 추가

---
+++ backend/src/main/java/OUA/OUA_V1/order/domain/Order.java
@@ -0,0 +1,51 @@
+package OUA.OUA_V1.order.domain;
+
+import OUA.OUA_V1.BaseEntity;
+import OUA.OUA_V1.auth.util.SecureResource;
+import OUA.OUA_V1.member.domain.Member;
+import OUA.OUA_V1.product.domain.Product;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Table(name = "orders")
+public class Order extends BaseEntity implements SecureResource {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(nullable = false)
+    private int orderPrice;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    public Order(Member member, Product product, int orderPrice) {
+        this.member = member;
+        this.product = product;
+        this.orderPrice = orderPrice;
+        this.status = OrderStatus.PENDING;
+    }
+
+    @Override
+    public boolean isAuthorizedBy(Member member) {
+        return this.member.equals(member);
+    }
+}
