1fcb8f408d031441501dbdbef2047da7b24a4825
이준호 <ghzm888@gmail.com>
2025-04-28 15:26:58 +0900

    fix: 입찰가 예외처리 버그 수정

--- backend/src/main/java/OUA/OUA_V1/advice/badRequest/OrderPriceException.java
+++ backend/src/main/java/OUA/OUA_V1/advice/badRequest/OrderPriceException.java
@@ -2,7 +2,7 @@ package OUA.OUA_V1.advice.badRequest;
 
 public class OrderPriceException extends BadRequestException {
 
-    private static final String MESSAGE_NOTICE_MIN_MAX = "%s은(는) 최소 %d, 최대 %d입니다. 현재 입력 가격: %d";
+    private static final String MESSAGE_NOTICE_MIN_MAX = "입찰 가격은 최소 %d원, 최대 %d원 사이여야 합니다. 현재 입력 가격: %d원";
 
     public OrderPriceException(int minPrice, int maxPrice, int currentPrice) {
         super(String.format(MESSAGE_NOTICE_MIN_MAX, minPrice, maxPrice, currentPrice));
