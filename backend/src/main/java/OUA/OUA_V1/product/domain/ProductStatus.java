c9795683558ec1f5b5ad325434e9ee34bd778884
이준호 <ghzm888@gmail.com>
2025-05-02 17:05:16 +0900

    refactor: 기존 onSale 컬럼을 enum Status로 변경

---
+++ backend/src/main/java/OUA/OUA_V1/product/domain/ProductStatus.java
@@ -0,0 +1,10 @@
+package OUA.OUA_V1.product.domain;
+
+public enum ProductStatus {
+    ACTIVE,     // 판매 중
+    ENDED,      // 정상 종료
+    SOLD,       // 낙찰 완료
+    UNSOLD,     // 유찰
+    CANCELED,   // 판매자 취소
+    DELETED     // 관리자 삭제
+}
