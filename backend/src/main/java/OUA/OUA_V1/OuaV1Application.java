485c3244131e8b527f02c933c88e110a9d22557a
이준호 <ghzm888@gmail.com>
2025-06-13 21:33:53 +0900

    refactor: 기존 Redis 스케줄러 -> Spring 스케줄링으로 변경

--- backend/src/main/java/OUA/OUA_V1/OuaV1Application.java
+++ backend/src/main/java/OUA/OUA_V1/OuaV1Application.java
@@ -4,14 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableJpaAuditing
+@EnableScheduling
 public class OuaV1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OuaV1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OuaV1Application.class, args);
+    }
 
 }
